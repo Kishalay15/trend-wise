9f0456196281b26e27d77b409d42c9a1163572e4
Kishalay15 <kishalaylahiri@gmail.com>
2025-06-30 23:16:23 +0530

    updated the render-build

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Install Chromium
-apt-get update
-apt-get update && apt-get install -y chromium-browser
+apt-get update && apt-get install -y chromium
+
+chromium --version
 
 # Install Node.js dependencies
 npm install
